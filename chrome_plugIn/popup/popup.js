3eedbe2ee288bd04b72c26162aa34e6a76f795b4
haihu <haihu@tcl.com>
2016-04-11 20:06:53 +0800

    4/44

---
+++ chrome_plugIn/popup/popup.js
@@ -0,0 +1,99 @@
+
+
+window.pubAttributes={};
+$('.switch-checkbox').bootstrapSwitch({
+    onText:"打开",
+    offText:"关闭",
+    size:"small",
+    onColor:"success"
+});
+//注册开关事件
+$('.switch-checkbox').on('switchChange.bootstrapSwitch', function (e, data) {
+    console.log(data);
+    chrome.storage.sync.set({open:data},function(){console.log("操作成功了")});
+});
+
+chrome.storage.sync.get("open",function(obj){
+    //判断status
+    var open=obj.open;
+     $('.switch-checkbox').bootstrapSwitch('state',open)
+
+});
+
+
+//展示被屏蔽的人
+
+function showBlock(){
+    chrome.storage.sync.get("blocked",function(obj){
+        if(obj.blocked==undefined){
+            $(".blockedUser ul").html("暂无被屏蔽的ID");
+            return
+        }
+        //判断status
+        window.pubAttributes.blocked=[];
+        var blockArray=obj.blocked;
+        var length=blockArray.length;
+        var closeSpan='<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>';
+        if(length!=0){
+            //不为0
+            var blockedUser=""
+            _.each(blockArray,function(element,index){
+                var solo='<li class="block-solo">'+element+closeSpan+'</li>';
+                blockedUser=blockedUser+solo
+            });
+            $(".blockedUser ul").html(blockedUser);
+            window.pubAttributes.blocked=blockArray;
+        }else{
+            $(".blockedUser ul").html("没有人被屏蔽!")
+        }
+
+    });
+}
+
+showBlock()
+
+$("#submit").on("click",function(){
+    //处理点击
+    var getData=getInput();
+    if(getData){
+        console.log("最后的数组是")
+        console.log(getData);
+        chrome.storage.sync.set({blocked:getData},function(){
+            $(".infoBar").removeClass("alert-warning").addClass("alert-success").html("保存成功").slideDown(300)
+            setTimeout(function(){
+                $(".infoBar").slideUp()
+            },2000);
+            showBlock();
+            //清空输入框
+            $("#inputUser").val("")
+        });
+
+    }
+
+});
+
+function getInput(){
+    //处理输入
+    var inputData=$("#inputUser").val().trim();
+    var inputToArray=inputData.split(/[,，]/);
+    //循环遍历每个Array值，清理格式
+    _.each(inputToArray,function(ele,index){
+        inputToArray[index]=ele.trim();
+    });
+    //去除false值
+    inputToArray= _.compact(inputToArray);
+    if(inputToArray.length==0){
+        $(".infoBar").removeClass("alert-success").addClass("alert-warning").html("你没有输入数据").slideDown(300)
+        setTimeout(function(){
+            $(".infoBar").slideUp()
+        },2000);
+        return false
+    }
+    var arrayConact=inputToArray.concat(window.pubAttributes.blocked);
+    //清理重复的值
+    var endArray=_.uniq(arrayConact);
+    return endArray
+    //
+}
+
+
--- chrome_plugIn/process.js
+++ chrome_plugIn/process.js
@@ -1,59 +1,81 @@
 /**
  * Created by huhai on 2016/4/7.
  */
+//先检查屏蔽功能的开关
+chrome.storage.sync.get("open",function(obj){
+    //判断status
+    var open=obj.open;
+    if(!open){
+        //如果处于关闭状态
+       return
+    }else{
+        //打开状态
+        doFilter()
+    }
+});
+
+//chrome.storage.sync.set({blocked:['cccyer','拖鞋抽脸','飞鹰玩姑娘','e800','cocoss','希尔伯特大弟子']},function(){console.log("保存成功")});
+
 
 //chrome.runtime.sendMessage("huhai");
 //判断是什么类型的页面
-//
-    var blockUsers=['cccyer','拖鞋抽脸','飞鹰玩姑娘','e800'];
-var href=window.location.href;
-var detail=/viewthread/.test(href);
-if(detail){
-        //如果是帖子详情页面
-        filterPost()
-}else{
-    //如果是帖子列表页面
-    filterDataList()
+function doFilter(){
+    var blockUsers;
+    chrome.storage.sync.get("blocked",function(obj){
 
-}
+        blockUsers=obj.blocked;
+        if(blockUsers.length==0) return
+        var href=window.location.href;
+        var detail=/viewthread/.test(href);
+        if(detail){
+            //如果是帖子详情页面
+            filterPost()
+        }else{
+            //如果是帖子列表页面
+            filterDataList()
 
-
-function filterDataList(){
-    $("table.datatable td.author a").each(function(){
-        var username=$(this).html();
-        //检查blockUsers里面是否有这个user
-        var checkIfExists= filterName(username);
-        if(checkIfExists){
-            var parents=$(this).parents("tbody[id^='normalthread']");
-            parents.remove()
         }
-
     })
 
-}
+
+
+    function filterDataList(){
+        $("table.datatable td.author a").each(function(){
+            var username=$(this).html();
+            //检查blockUsers里面是否有这个user
+            var checkIfExists= filterName(username);
+            if(checkIfExists){
+                var parents=$(this).parents("tbody[id^='normalthread']");
+                parents.remove()
+            }
+
+        })
+
+    }
 //------------------------------------------------
-function filterPost(){
-    //遍历每一个a
-    $("#postlist .postinfo a").each(function(){
-        //
-        var username=$(this).html();
-        //检查blockUsers里面是否有这个user
-        var checkIfExists= filterName(username);
-        if(checkIfExists){
-            var parents=$(this).parents("div[id^='post_']");
-            parents.remove()
+    function filterPost(){
+        //遍历每一个a
+        $("#postlist .postinfo a").each(function(){
+            //
+            var username=$(this).html();
+            //检查blockUsers里面是否有这个user
+            var checkIfExists= filterName(username);
+            if(checkIfExists){
+                var parents=$(this).parents("div[id^='post_']");
+                parents.remove()
+            }
+        })
+    }
+
+
+    function filterName(name){
+        var checkIfExists= _.indexOf(blockUsers,name);
+        if(checkIfExists==-1){
+            //代表不存在
+            return false;
+        }else{
+            //代表存在
+            return true;
         }
-    })
-}
-
-
-function filterName(name){
-    var checkIfExists= _.indexOf(blockUsers,name);
-    if(checkIfExists==-1){
-        //代表不存在
-        return false;
-    }else{
-        //代表存在
-       return true;
     }
 }
