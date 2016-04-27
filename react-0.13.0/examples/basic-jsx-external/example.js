461521546ffcbe97e89e6bad8e32b5284773590e
huhai <huhai007@gmail.com>
2016-04-27 22:29:04 +0800

    touch事件写成的silder

--- react-0.13.0/examples/basic-jsx-external/example.js
+++ react-0.13.0/examples/basic-jsx-external/example.js
@@ -1,5 +1,6 @@
 var ExampleApplication = React.createClass({
   render: function() {
+    console.log(this)
     var elapsed = Math.round(this.props.elapsed  / 100);
     var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
     var message =
@@ -13,7 +14,7 @@ var start = new Date().getTime();
 
 setInterval(function() {
   React.render(
-    <ExampleApplication elapsed={new Date().getTime() - start} />,
+    <ExampleApplication elapsed={new Date().getTime() - start}  name="huhai" xx='yy'/>,
     document.getElementById('container')
   );
 }, 50);
